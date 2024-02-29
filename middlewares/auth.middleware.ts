5f36217bc0ef205e6033c0dc45a0810fbcb449e4
Russell Peiris <ardpeiris@gmail.com>
2024-02-29 14:48:45 +0530

    auth

---
+++ middlewares/auth.middleware.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import User from '../schemas/user.schema'
+
+export async function authenticate(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const token = req.cookies.jwt
+    if (!token) {
+      return res.status(401).json({ message: 'Please login to continue' })
+    }
+
+    const jwtSecret = process.env.JWT_SECRET || ''
+    const decoded = jwt.verify(token, jwtSecret) as JwtPayload
+    if (!decoded || !decoded.userId) {
+      return res.status(401).json({ message: 'You are not authorized' })
+    }
+
+    const user = await User.findOne({ _id: decoded.userId })
+    if (!user) {
+      res.status(401).json({ message: 'Unauthorized, username not found' })
+    }
+    next()
+  } catch (error: any) {
+    console.error('Failed to authenticate user', error.message)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+}
---
+++ schemas/user.schema.ts
@@ -0,0 +1,37 @@
+import bycrypt from 'bcrypt'
+import { Schema, model } from 'mongoose'
+import { hashPassword } from '../utils/hash.util'
+
+export interface IUser extends Document {
+  username: string
+  password: string
+  comparePassword(password: string): Promise<boolean>
+}
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+userSchema.pre('save', async function (next) {
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await hashPassword(user.password)
+  }
+  next()
+})
+
+userSchema.methods.comparePassword = async function (password: string) {
+  return await bycrypt.compare(password, this.password)
+}
+
+const User = model<IUser>('User', userSchema)
+
+export default User
